29d726fd2fbdacfb2e5967604eecd1fdd1abc95b
ArkaOnOff <arka.on.ytb@gmail.com>
2024-09-15 20:48:46 +0200

    First Commit

---
+++ src/client/java/com/drarka/RandomKeysClient.java
@@ -0,0 +1,10 @@
+package com.drarka;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RandomKeysClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
